b386cc2eb66408add6a34e6b685f281433a95404
dreamnajababy <dream.patnarin@maqe.com>
2022-07-14 14:34:10 +0700

    feat(tutorial): reset

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { BlogController } from '@/blog/blog.controller'
 import { BlogService } from '@/blog/blog.service'
-import { Blog } from '@/blog/entities/blog.entity'
 import { BlogRepository } from '@/blog/interfaces/blog.repository'
 import { FakeBlogRepository } from '@/blog/repositories/fake-blog.repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Blog])],
   controllers: [BlogController],
   providers: [
     BlogService,
--- src/blog/entities/blog.entity.ts
+++ src/blog/entities/blog.entity.ts
@@ -1,15 +1,8 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm"
 
-@Entity()
 export class  Blog  {
-  @PrimaryGeneratedColumn()
   id: number
-  @Column()
   title: string
-  @Column()
   content: string
-  @CreateDateColumn()
   createdAt: Date | string
-  @Column({ type: 'json', nullable: true })
   tags: string[]
 }
