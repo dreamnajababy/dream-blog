7d9d4ae8ed3cb1ccdf7b350f7cb9f13cc2623556
dreamnajababy <dream.patnarin@maqe.com>
2022-07-14 14:24:52 +0700

    config(blog-postgres): add typeORM forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from '@/blog/blog.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'example',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     BlogModule,
   ],
 })
